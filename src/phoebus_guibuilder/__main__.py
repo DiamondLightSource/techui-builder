1ec424676251c8fb75ae872653eb2928c3dd4ac9
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-02-14 11:54:56 +0000

    Various updates to try and get ci to pass with a more consistent env on github actions

--- src/phoebus_guibuilder/__main__.py
+++ src/phoebus_guibuilder/__main__.py
@@ -3,7 +3,7 @@
 from argparse import ArgumentParser
 from collections.abc import Sequence
 
-from . import __version__
+from . import __version__, guibuilder
 
 __all__ = ["main"]
 
@@ -11,13 +11,16 @@ __all__ = ["main"]
 def main(args: Sequence[str] | None = None) -> None:
     """Argument parser for the CLI."""
     parser = ArgumentParser()
+    parser.add_argument("filename", help="The path to create_gui.yaml")
     parser.add_argument(
         "-v",
         "--version",
         action="version",
         version=__version__,
     )
-    parser.parse_args(args)
+    _args = parser.parse_args(args)
+
+    guibuilder.main(_args.filename)
 
 
 if __name__ == "__main__":

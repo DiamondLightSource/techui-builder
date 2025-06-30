1f6a0154388dfe7f64fd8c14c70f3f4ed840271b
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-06-30 12:33:53 +0000

    Fix tests

--- tests/test_guibuilder.py
+++ tests/test_guibuilder.py
@@ -23,21 +23,21 @@ def test_guibuilder(gb):
 
 def test_gb_extract_services(gb):
     gb.find_services_folders()
-    assert gb.valid_entities[0].type == "pmac.autohome"
-    assert gb.valid_entities[0].DESC == "motor"
-    assert gb.valid_entities[0].P == "BL01T-MO-MAP-01:STAGE"
-    assert gb.valid_entities[0].M is None
-    assert gb.valid_entities[0].R is None
-    assert gb.valid_entities[1].type == "pmac.dls_pmac_asyn_motor"
-    assert gb.valid_entities[1].DESC == "motor"
-    assert gb.valid_entities[1].P == "BL01T-MO-MAP-01:STAGE"
-    assert gb.valid_entities[1].M == "X"
-    assert gb.valid_entities[1].R is None
-    assert gb.valid_entities[2].type == "pmac.dls_pmac_asyn_motor"
-    assert gb.valid_entities[2].DESC == "motor"
-    assert gb.valid_entities[2].P == "BL01T-MO-MAP-01:STAGE"
-    assert gb.valid_entities[2].M == "A"
-    assert gb.valid_entities[1].R is None
+    assert gb.entities[0].type == "pmac.autohome"
+    assert gb.entities[0].DESC == "motor"
+    assert gb.entities[0].P == "BL01T-MO-MAP-01:STAGE"
+    assert gb.entities[0].M is None
+    assert gb.entities[0].R is None
+    assert gb.entities[1].type == "pmac.dls_pmac_asyn_motor"
+    assert gb.entities[1].DESC == "motor"
+    assert gb.entities[1].P == "BL01T-MO-MAP-01:STAGE"
+    assert gb.entities[1].M == "X"
+    assert gb.entities[1].R is None
+    assert gb.entities[2].type == "pmac.dls_pmac_asyn_motor"
+    assert gb.entities[2].DESC == "motor"
+    assert gb.entities[2].P == "BL01T-MO-MAP-01:STAGE"
+    assert gb.entities[2].M == "A"
+    assert gb.entities[1].R is None
 
 
 def test_gui_map(gb):

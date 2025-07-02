198319de9cbca584887a94805361404b5eeda518
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-07-02 08:01:46 +0000

    Fixed missed renaming of things

--- example-synoptic/test_bobscreens.py
+++ example-synoptic/test_bobscreens.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from pprint import PrettyPrinter
 
-from phoebus_guibuilder import BobScreens, Guibuilder
+from techui_builder import BobScreens, Builder
 
 pp = PrettyPrinter()
 
@@ -9,7 +9,7 @@ dom = "BL23B"
 
 bob_file = Path(f"data/{dom}-synoptic-src.bob")
 
-gui = Guibuilder()
+gui = Builder()
 
 bob_screen = BobScreens(bob_file)
 bob_screen.read_bob()

e4cf1ef6139c717507b68cf85d28f6866002a350
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-07-21 14:22:20 +0000

    Rework services directory logic to look for blxxy-services by default
    Stores service directory path in self_service_dir which allows for
    overwriting in test scripts

--- example-synoptic/generate_synoptic.py
+++ example-synoptic/generate_synoptic.py
@@ -14,6 +14,9 @@ create_gui_file = Path("example-synoptic/create_gui.yaml")
 
 gui = Builder(create_gui=create_gui_file)
 
+# Overwrite after initialised to make sure this is picked up
+gui._services_dir = Path("example-synoptic/bl23b-services")  # noqa: SLF001
+
 gui.setup()
 
 # TODO: figure out the in-between steps

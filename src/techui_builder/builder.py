7f9838389726813382dff0f3a113f2bd5b5fc798
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2025-07-25 14:22:00 +0000

    Made variables private

--- src/techui_builder/builder.py
+++ src/techui_builder/builder.py
@@ -32,17 +32,17 @@ class Builder:
     components: list[Component] = field(default_factory=list, init=False)
     entities: list[Entry] = field(default_factory=list, init=False)
 
-    services_dir: Path = field(init=False, repr=False)
+    _services_dir: Path = field(init=False, repr=False)
     _gui_map: dict = field(init=False, repr=False)
-    write_directory: Path = field(init=False, repr=False)
+    _write_directory: Path = field(init=False, repr=False)
 
     def __post_init__(self):
         # Populate beamline and components
         self._extract_from_create_gui()
 
-        # Get list of services from services_directory
+        # Get list of services from _services_directory
         # Requires beamline has already been read from create_gui.yaml
-        self.services_dir = Path(f"{self.beamline.dom}-services")
+        self._services_dir = Path(f"{self.beamline.dom}-services")
 
         self._read_gui_map()
 
@@ -83,7 +83,7 @@ class Builder:
             # If service doesn't exist, file open will fail throwing exception
             try:
                 self._extract_entities(
-                    ioc_yaml=f"{self.services_dir}/services/{service_name}/config/ioc.yaml",
+                    ioc_yaml=f"{self._services_dir}/services/{service_name}/config/ioc.yaml",
                     component=component,
                 )
                 self._generate_screen(screen_name=component.name)
@@ -132,7 +132,7 @@ class Builder:
     def _generate_screen(self, screen_name: str):
         generator = Generator(self.entities, self._gui_map, screen_name)
         generator.build_groups()
-        generator.write_screen(self.write_directory)
+        generator.write_screen(self._write_directory)
 
     def _generate_json_map(
         self, file_path: Path, visited: set[Path] | None = None

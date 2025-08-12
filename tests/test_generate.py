8f07e627daaec31ece415425e0ca22e380c4c81e
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2025-08-12 07:28:50 +0000

    Added tests for generate.py

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -3,13 +3,12 @@ from pathlib import Path
 import pytest
 
 from techui_builder.builder import Builder
-
-# from techui_builder.generate import Generator
+from techui_builder.generate import Generator
 
 
 @pytest.fixture
 def gb():
-    b = Builder(Path("example/create_gui.yaml)"))
+    b = Builder(Path("example/create_gui.yaml"))
     b._services_dir = Path(f"./example/{b.beamline.dom}-services")
     b._write_directory = Path("example/")
     b._extract_entities(
@@ -18,11 +17,13 @@ def gb():
     return b
 
 
-# def test_build_groups(gb: Builder):
-#     generator = Generator(
-#         gb.entities, gb._gui_map, gb.components[4].name
-#     )  # TODO: remove hardcoded index
-#     generator.build_groups()
-#     with open("./tests/test_files/group.xml") as f:
-#         control = f.read()
-#     assert str(generator.group) == control
+def test_build_groups(gb: Builder):
+    generator = Generator(
+        gb.entities["BL01T-MO-MAP-01:STAGE"], gb.components[4].name, Path("./")
+    )  # TODO: remove hardcoded index
+    generator.build_groups()
+    print(generator.group)
+    print("------------HERE-------------")
+    with open("./tests/test_files/group.xml") as f:
+        control = f.read()
+    assert str(generator.group) == control

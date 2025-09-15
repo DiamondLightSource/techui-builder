cd0cabe2313d086a892505ba921c1b2b3e9e0283
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-09-15 14:04:21 +0000

    Converted json_map to an object for (hopefully) easier to understand code
    Rename next_node to child_node
    
    Create custom serialise function to ignore default values in the json_map dataclass
    
    Include checks for default_factory default values

--- src/techui_builder/builder.py
+++ src/techui_builder/builder.py
@@ -1,8 +1,7 @@
 import json
 import logging
 from collections import defaultdict
-from collections.abc import MutableMapping
-from dataclasses import dataclass, field
+from dataclasses import _MISSING_TYPE, dataclass, field
 from pathlib import Path
 from typing import Any
 
@@ -13,9 +12,17 @@ from lxml.objectify import ObjectifiedElement
 from techui_builder.generate import Generator
 from techui_builder.objects import Beamline, Component, Entity
 
-# Recursive type for Json map file
-type json_map = MutableMapping[str, str | list["json_map"]]
 LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class json_map:
+    file: str
+    exists: bool = True
+    duplicate: bool = False
+    children: list["json_map"] = field(default_factory=list)
+    macros: dict[str, str] = field(default_factory=dict)
+    error: str = ""
 
 
 @dataclass
@@ -155,16 +162,15 @@ files in services"
         if visited is None:
             visited = set()
 
+        current_node = json_map(str(screen_path))
+
         abs_path = screen_path
         dest_path = dest_path
         if abs_path in visited:
-            return {
-                "file": str(screen_path),
-                "note": "Already visited (cycle detected)",
-            }
-
+            current_node.exists = True
+            current_node.duplicate = True
+            return current_node
         visited.add(abs_path)
-        node: json_map = {"file": str(screen_path), "children": []}
 
         try:
             tree: etree._ElementTree = objectify.parse(abs_path, etree.XMLParser())
@@ -190,38 +196,30 @@ files in services"
                         "macros", namespaces=None
                     )
                     if macros is not None:
-                        p: ObjectifiedElement | None = macros.find(
-                            ".//P", namespaces=None
-                        )
-                        m: ObjectifiedElement | None = macros.find(
-                            ".//M", namespaces=None
-                        )
-                        if p is not None and p.text:
-                            macro_dict["P"] = p.text
-                        if m is not None and m.text:
-                            macro_dict["M"] = m.text
+                        for macro in macros:
+                            macro_dict[macro.tag] = macro.text  # type: ignore
 
                 # Crawl the next file
                 if next_file_path.is_file():
                     # TODO: investigate non-recursive approaches?
-                    next_node = self._generate_json_map(
+                    child_node = self._generate_json_map(
                         next_file_path, dest_path, visited
                     )
                 else:
-                    next_node = {"file": str(file_path), "error": "File not found"}
+                    child_node = json_map(str(file_path), exists=False)
 
-                next_node.update(macro_dict)
+                child_node.macros = macro_dict
                 # TODO: make this work for only list[json_map]
-                assert isinstance(node["children"], list)
+                assert isinstance(current_node.children, list)
                 # TODO: fix typing
-                node["children"].append(next_node)  # type: ignore
+                current_node.children.append(child_node)
 
         except etree.ParseError as e:
-            node["error"] = f"XML parse error: {e}"
+            current_node.error = f"XML parse error: {e}"
         except Exception as e:
-            node["error"] = str(e)
+            current_node.error = str(e)
 
-        return node
+        return current_node
 
     def write_json_map(
         self,
@@ -239,4 +237,30 @@ files in services"
 
         map = self._generate_json_map(synoptic, dest)
         with open(dest.joinpath("json_map.json"), "w") as f:
-            json.dump(map, f, indent=4)
+            f.write(json.dumps(map, indent=4, default=lambda o: _serialise_json_map(o)))
+
+
+# Function to convert the json_map objects into dictionaries,
+# while ignoring default values
+def _serialise_json_map(map: json_map) -> dict[str, Any]:
+    def _check_default(key: str, value: Any):
+        # Is a default factory used? (e.g. list, dict, ...)
+        if not isinstance(
+            json_map.__dataclass_fields__[key].default_factory, _MISSING_TYPE
+        ):
+            # If so, check if value is the same as default factory
+            default = json_map.__dataclass_fields__[key].default_factory()
+        else:
+            # If not, check if value is the default value
+            default = json_map.__dataclass_fields__[key].default
+        return value == default
+
+    # Loop over everything in the json map object's dictionary
+    for val in map.__dict__.values():
+        # If a value is another nedted json_map object, serialise that too
+        if isinstance(val, json_map):
+            val = _serialise_json_map(val)
+
+    # only include any items if they are not the default value
+    d = {k: v for (k, v) in map.__dict__.items() if not _check_default(k, v)}
+    return d

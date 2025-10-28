b3d61ab2cda464d3770dbff19ff94ba4a57f493c
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2025-10-28 11:43:10 +0000

    modified regex to enable parsing [], [,], *{part_PV}*

--- src/techui_builder/objects.py
+++ src/techui_builder/objects.py
@@ -64,25 +64,25 @@ prefix={self.P}, suffix={self.R}, filename={self.file})"
             r"""
             ^           # start of string
             (?=         # lookahead to ensure the following pattern matches
-                [A-Za-z0-9-\*\?\[\]]{13,16} # match 13-16 alphanumeric chars,-/wildcards
-                [:A-Za-z0-9\*\?\[\]]* # match >=0 colons or alphanumeric chars/wildcards
-                [.A-Za-z0-9\*\?\[\]]  # match a dot or alphanumeric chars or wildcards
+                [A-Za-z0-9*?\[\]-]{2,16} # match 13-16 alphanumeric chars,-/wildcards
+                [:A-Za-z0-9*?[\]]* # match >=0 colons or alphanumeric chars/wildcards
+                [.A-Za-z0-9*?[\]]  # match a dot or alphanumeric chars or wildcards
             )
             (?!.*--)    # negative lookahead to ensure no double hyphens
             (?!.*:\..)  # negative lookahead to ensure no colon followed by a dot
             (           # start of capture group 1
-                (?:[A-Za-z0-9]{2,5}-){3} # match 2 to 5 alphanumeric characters followed
+                (?:[A-Za-z0-9*?,\[\]-])* # match 2 to 5 alphanumeric characters followed
                                     # by a hyphen, repeated 3 times
-                [\d]*   # match zero or more digits
+                [\d*]*   # match zero or more digits
                 [^:]?   # match zero or one non-colon character
             )
-            (?::([a-zA-Z0-9:]*))? # match zero or one colon followed by zero or more
+            (?::([a-zA-Z0-9*:]*))? # match zero or one colon followed by zero or more
                                 # alphanumeric characters or colons (capture group 2)
-            (?:\.([a-zA-Z0-9]+))? # match zero or one dot followed by one or more
+            (?:\.([a-zA-Z0-9*]+))? # match zero or one dot followed by one or more
                                 # alphanumeric characters (capture group 3)
             $           # end of string
         """,
-            re.VERBOSE,
+            re.VERBOSE | re.UNICODE,
         )
 
         match = re.match(pattern, self.prefix)

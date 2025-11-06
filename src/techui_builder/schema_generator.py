6b786fcb6ae6662a72bb98596cfd1272b4843025
Sode, Adedamola (DLSLtd,RAL,LSCI) <adedamola.sode@diamond.ac.uk>
2025-11-06 11:51:38 +0000

    Changed the schemas to json files, and ibek_mapping to techui_support

--- src/techui_builder/schema_generator.py
+++ src/techui_builder/schema_generator.py
@@ -1,6 +1,5 @@
+import json
 from pathlib import Path
-
-import yaml
 
 from techui_builder.models import (
     GuiComponents,
@@ -11,18 +10,18 @@ SCHEMAS_DIR = Path("schemas")
 SCHEMAS_DIR.mkdir(exist_ok=True)
 
 
-def write_yaml_schema(model_name: str, schema_dict: dict) -> None:
-    out = SCHEMAS_DIR / f"{model_name}.schema.yml"
+def write_json_schema(model_name: str, schema_dict: dict) -> None:
+    out = SCHEMAS_DIR / f"{model_name}.schema.json"
     with out.open("w", encoding="utf-8") as f:
-        yaml.safe_dump(schema_dict, f, sort_keys=False)
+        json.dump(schema_dict, f, sort_keys=False)
     print(f"✅ Wrote {out}")
 
 
 def schema_generator() -> None:
     # techui
     tu = TechUi.model_json_schema()
-    write_yaml_schema("techui", tu)
+    write_json_schema("techui", tu)
 
     # ibek_mapping
-    ibek = GuiComponents.model_json_schema()
-    write_yaml_schema("ibek_mapping", ibek)
+    tu_support = GuiComponents.model_json_schema()
+    write_json_schema("techui.support", tu_support)

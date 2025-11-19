eeb184c89c11721f7c66ba5999f29f6949756084
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-11-19 10:40:49 +0000

    Add basic validate check function

--- src/techui_builder/validator.py
+++ src/techui_builder/validator.py
@@ -1,6 +1,8 @@
 import logging
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
+
+from lxml import etree
 
 LOGGER = logging.getLogger(__name__)
 
@@ -8,3 +10,14 @@ LOGGER = logging.getLogger(__name__)
 @dataclass
 class Validator:
     path: Path
+    validate: bool = field(default=False, init=False, repr=False)
+
+    def check_bob(self):
+        # etree has to used as objectify ignore comments
+        xml = etree.parse(self.path)
+        # fetch all the comments at the base of the tree
+        comments = list(xml.getroot().itersiblings(tag=etree.Comment, preceding=True))
+        if len(comments) > 0:
+            # Check if any comments found are the manually saved tag
+            if any(str(comment).startswith("<!--Saved on ") for comment in comments):
+                self.validate = True

4dcfea1739c0110034fe4d98f7142ea6e26855dd
Ollie Copping <oliver.copping@diamond.ac.uk>
2025-12-10 15:08:41 +0000

    Add basic Renderer class

---
+++ src/techui_builder/render.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader
+
+from techui_builder.models import Beamline
+
+
+@dataclass
+class Renderer:
+    support_screen_path: Path
+    screen_path: Path
+    beamline: Beamline
+
+    def __post_init__(self):
+        self.env = Environment(loader=FileSystemLoader(self.support_screen_path))
+
+    def load_screen(self):
+        self.screen_template = self.env.get_template(self.screen_path.name)

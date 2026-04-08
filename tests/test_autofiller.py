19cbb0fe5afe2c69e0dbc4bfc7a6973aed454a9a
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-04-08 08:16:16 +0000

    Tidy patches in tests (move most to be decorators)

--- tests/test_autofiller.py
+++ tests/test_autofiller.py
@@ -9,14 +9,14 @@ from lxml.objectify import Element
 from techui_builder.models import Component
 
 
-def test_autofiller_read_bob(autofiller):
-    # Imported in to autofill from utils, so that needs to be patched
-    with patch("techui_builder.autofill.read_bob") as mock_read_bob:
-        mock_read_bob.return_value = (Mock(spec=ElementTree), Mock())
+# Imported in to autofill from utils, so that needs to be patched
+@patch("techui_builder.autofill.read_bob")
+def test_autofiller_read_bob(mock_read_bob, autofiller):
+    mock_read_bob.return_value = (Mock(spec=ElementTree), Mock())
 
-        autofiller.read_bob()
+    autofiller.read_bob()
 
-        mock_read_bob.assert_called()
+    mock_read_bob.assert_called()
 
 
 def test_autofiller_autofill_bob(autofiller):
@@ -34,22 +34,20 @@ def test_autofiller_autofill_bob(autofiller):
     assert mock_widget.find("run_actions_on_mouse_click") == "true"
 
 
-def test_autofiller_write_bob(autofiller):
-    with (
-        patch("techui_builder.builder.etree.ElementTree") as mock_tree,
-        patch("techui_builder.builder.objectify.deannotate") as mock_deannotate,
-    ):
-        autofiller.tree = mock_tree
+@patch("techui_builder.builder.objectify.deannotate")
+@patch("techui_builder.builder.etree.ElementTree")
+def test_autofiller_write_bob(mock_tree, mock_deannotate, autofiller):
+    autofiller.tree = mock_tree
 
-        autofiller.write_bob(Path("tests/test_files/test_autofilled_bob.bob"))
+    autofiller.write_bob(Path("tests/test_files/test_autofilled_bob.bob"))
 
-        mock_deannotate.assert_called_once()
-        mock_tree.write.assert_called_once_with(
-            Path("tests/test_files/test_autofilled_bob.bob"),
-            pretty_print=True,
-            encoding="utf-8",
-            xml_declaration=True,
-        )
+    mock_deannotate.assert_called_once()
+    mock_tree.write.assert_called_once_with(
+        Path("tests/test_files/test_autofilled_bob.bob"),
+        pretty_print=True,
+        encoding="utf-8",
+        xml_declaration=True,
+    )
 
 
 @pytest.mark.parametrize(
@@ -74,7 +72,9 @@ def test_autofiller_write_bob(autofiller):
         ),
     ],
 )
+@patch("techui_builder.autofill._get_action_group")
 def test_autofiller_replace_content(
+    mock_get,
     autofiller,
     example_related_widget,
     prefix,
@@ -84,49 +84,48 @@ def test_autofiller_replace_content(
     expected_desc,
     expected_file,
 ):
-    with patch("techui_builder.autofill._get_action_group") as mock_get:
-        mock_get.return_value = example_related_widget.actions.action
+    mock_get.return_value = example_related_widget.actions.action
 
-        # Cannot use a Mock object as need P to be computed
-        fake_component = Component(
-            prefix=prefix,
-            desc=description,
-            file=filename,
-            macros=macros,
-        )
+    # Cannot use a Mock object as need P to be computed
+    fake_component = Component(
+        prefix=prefix,
+        desc=description,
+        file=filename,
+        macros=macros,
+    )
 
-        autofiller.replace_content(
-            example_related_widget,
-            "test_component",
-            fake_component,
-        )
+    autofiller.replace_content(
+        example_related_widget,
+        "test_component",
+        fake_component,
+    )
 
-        assert example_related_widget.pv_name == f"{prefix}:STA"
-        assert example_related_widget.actions.action.description.text == expected_desc
-        assert example_related_widget.actions.action.file.text == expected_file
-        if macros is not None:
-            for k, v in macros.items():
-                assert example_related_widget.actions.action.macros[k] == macros[k] == v
+    assert example_related_widget.pv_name == f"{prefix}:STA"
+    assert example_related_widget.actions.action.description.text == expected_desc
+    assert example_related_widget.actions.action.file.text == expected_file
+    if macros is not None:
+        for k, v in macros.items():
+            assert example_related_widget.actions.action.macros[k] == macros[k] == v
 
 
-def test_autofiller_replace_content_no_action_group(autofiller, caplog):
+@patch("techui_builder.autofill._get_action_group")
+def test_autofiller_replace_content_no_action_group(mock_get, autofiller, caplog):
     # Just to only run the code we want to test
     autofiller.macros = ["desc"]
 
-    with patch("techui_builder.autofill._get_action_group") as mock_get:
-        # Simulate no action group found
-        mock_get.return_value = None
+    # Simulate no action group found
+    mock_get.return_value = None
 
-        mock_component = Mock(
-            spec=Component,
-            desc="description",
-        )
+    mock_component = Mock(
+        spec=Component,
+        desc="description",
+    )
 
-        with caplog.at_level(logging.DEBUG):
-            autofiller.replace_content(None, "", mock_component)
+    with caplog.at_level(logging.DEBUG):
+        autofiller.replace_content(None, "", mock_component)
 
-        for log_output in caplog.records:
-            assert "Skipping replace_content for" in log_output.message
+    for log_output in caplog.records:
+        assert "Skipping replace_content for" in log_output.message
 
 
 def test_autofiller_replace_content_unsupported_macro(autofiller):
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,15 +7,15 @@ from lxml.objectify import Element, ObjectifiedElement
 from techui_builder.utils import get_widgets, read_bob
 
 
-def test_read_bob():
-    with patch("techui_builder.utils.get_widgets") as mock_get_widgets:
-        mock_get_widgets.return_value = {"test_widget": Mock(spec=ObjectifiedElement)}
+@patch("techui_builder.utils.get_widgets")
+def test_read_bob(mock_get_widgets):
+    mock_get_widgets.return_value = {"test_widget": Mock(spec=ObjectifiedElement)}
 
-        tree, widgets = read_bob(Path("tests/test_files/index.bob"))
+    tree, widgets = read_bob(Path("tests/test_files/index.bob"))
 
-        assert isinstance(tree, _ElementTree)
-        assert isinstance(widgets["test_widget"], ObjectifiedElement)
-        mock_get_widgets.assert_called_once()
+    assert isinstance(tree, _ElementTree)
+    assert isinstance(widgets["test_widget"], ObjectifiedElement)
+    mock_get_widgets.assert_called_once()
 
 
 def test_get_widgets(example_symbol_widget):

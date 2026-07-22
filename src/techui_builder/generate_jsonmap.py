a0df6efd51088ca6510488b49c98a607fa37f1b6
Ollie Copping <oliver.copping@diamond.ac.uk>
2026-07-22 15:00:32 +0000

    Update JsonMapGenerator to handle .pvi.bob screens
    If one isn't found (e.g. when generated locally), it resorts to the old
    functionality

--- src/techui_builder/generate_jsonmap.py
+++ src/techui_builder/generate_jsonmap.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 from collections import defaultdict
 from dataclasses import _MISSING_TYPE, dataclass, field
 from pathlib import Path
@@ -11,9 +12,11 @@ from lxml import etree, objectify
 from lxml.objectify import ObjectifiedElement
 
 from techui_builder._logger import Logger
-from techui_builder.models import TechUi
+from techui_builder.models import Component, TechUi
 
 logger_ = logging.getLogger(__name__)
+
+_PVI_FILE_RE = re.compile(r"^(?:\$\(IOC\))\/([a-zA-Z]+[.a-zA-Z]+)$")
 
 
 def log_level(level: str):
@@ -54,6 +57,7 @@ class JsonMapGenerator:
     bob_path: Path = field(default=Path("index.bob"))
     techui: Path = field(default=Path("techui.yaml"))
     output: Path | None = field(default=None)
+    _current_service_name: str = field(default="", init=False, repr=False)
 
     def __post_init__(self):
         # Determine the directory to write the json map file to.
@@ -68,7 +72,7 @@ class JsonMapGenerator:
             self.techui == self.__class__.__dataclass_fields__["techui"].default
             and not self.techui.exists()
         ):
-            self.techui = self._parent_path.joinpath("techui.yaml")
+            self.techui = self._parent_path / "techui.yaml"
         try:
             self.techui_yaml: TechUi = TechUi.model_validate(
                 yaml.safe_load(self.techui.read_text(encoding="utf-8"))
@@ -102,7 +106,7 @@ class JsonMapGenerator:
             display_name = self._parse_display_name(display_name, file_path)
             return display_name
 
-        def _next_file_crawl(
+        def _child_file_crawl(
             file_path_text: str,
             destination_path: Path,
             name_element: str | None,
@@ -111,13 +115,21 @@ class JsonMapGenerator:
             macro_dictionary: dict[str, Any],
         ):
             # TODO: misleading var name?
-            next_file_path = destination_path.joinpath(file_path_text)
+            child_file_path = destination_path / file_path_text
+
+            match = _PVI_FILE_RE.fullmatch(file_path_text)
+            # The file path is a PVI screen, so attempt to find that screen
+            if match:
+                file_name = match.group(1)
+                child_file_path = (
+                    destination_path / f"../{self._current_service_name}/{file_name}"
+                )
 
             # Crawl the next file
-            if next_file_path.is_file():
+            if child_file_path.is_file():
                 # TODO: investigate non-recursive approaches?
                 child_node = self.generate_json_map(
-                    next_file_path,
+                    child_file_path,
                     destination_path,
                     current_component_name=component_name,
                     name_elem=name_element,
@@ -135,7 +147,11 @@ class JsonMapGenerator:
 
         # Create initial node at top of .bob file
         current_node = JsonMap(
-            str(screen_path.resolve().relative_to(self._parent_path.resolve())),
+            str(
+                screen_path.resolve().relative_to(
+                    self._parent_path.resolve(), walk_up=True
+                )
+            ),
             display_name=None,
         )
 
@@ -145,6 +161,11 @@ class JsonMapGenerator:
             and screen_path.stem in self.techui_yaml.components
         ):
             current_component_name = screen_path.stem
+            # We know from the if statement that it exists
+            _current_component = self.techui_yaml.components.get(current_component_name)
+            assert isinstance(_current_component, Component)
+            # TODO: How to find the screens if PV prefix is not the service name???
+            self._current_service_name = _current_component.prefix.lower()
 
         abs_path = screen_path.absolute()
 
@@ -214,7 +235,7 @@ class JsonMapGenerator:
                                 name_elem, current_component_name, file_path
                             )
 
-                            child_node = _next_file_crawl(
+                            child_node = _child_file_crawl(
                                 file_text,
                                 dest_path,
                                 name_elem,
@@ -249,7 +270,7 @@ class JsonMapGenerator:
                     name_elem, current_component_name, file_path
                 )
 
-                child_node = _next_file_crawl(
+                child_node = _child_file_crawl(
                     file_text,
                     dest_path,
                     name_elem,
